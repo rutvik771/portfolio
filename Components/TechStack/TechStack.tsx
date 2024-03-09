ef882635dd3f276aacae5eff106a8c58fcf0ef7b
rutvik771 <patelrutvik039@gmail.com>
2024-03-09 18:34:29 +0530

    fix type error in techstack file

--- Components/TechStack/TechStack.tsx
+++ Components/TechStack/TechStack.tsx
@@ -3,12 +3,14 @@ import Marquee from "react-fast-marquee";
 import { workData1 } from "./Data";
 
 export const TechStack = () => {
+  let speed:number = 60;
+  let gradientColor:any = [34, 38, 41];
   return (
     <div id='techstack'>
       <p className='heading'>Tech Stack</p>
       <div className='tech-container'>
         <div>
-          <Marquee speed="60" direction="left" gradientColor={[34, 38, 41]} gradientWidth={200}>
+          <Marquee speed={speed} direction="left" gradientColor={gradientColor} gradientWidth={200}>
             {workData1?.map((item) => (
               <div className='tech-box'>
                 <img className='stack-img' src={item?.image} />
