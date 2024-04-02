7f7a6df38ee4a880c2e0b2a2ddd5236cf3d48916
rutvik771 <patelrutvik039@gmail.com>
2024-04-02 09:58:42 +0530

    add new toolbar in text editor

--- Components/Editor/Editor.tsx
+++ Components/Editor/Editor.tsx
@@ -1,65 +1,55 @@
-import { useState ,useEffect} from "react";
-import dynamic from 'next/dynamic'; // Import dynamic from next/dynamic to load React Quill client-side
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic to load React Quill client-side
 import "react-quill/dist/quill.snow.css";
-import { IoMdUndo,IoMdRedo } from "react-icons/io";
+import EditorToolbar, { modules, formats } from "./EditorToolbar";
+import { IoMdUndo, IoMdRedo } from "react-icons/io";
 // Dynamic import ReactQuill to ensure it's only loaded on the client-side
-const ReactQuill = dynamic(() => import('react-quill'), { 
+const ReactQuill = dynamic(() => import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading Quill editor...</p>,
- });
+});
 
- // Define modules and formats
-const modules = {
-  toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-      [{size: []}],
-      [{ 'undo': <IoMdUndo/> }, { 'redo': <IoMdRedo /> }], // Specify custom icons for undo and redo buttons
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, 
-      {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image', 'video'],
-      ['clean'],
-  ],
-  clipboard: {
-      // toggle to add extra line breaks when pasting HTML:
-      matchVisual: false,
-  }
-};
+// Define modules and formats
+// const modules = {
+//   toolbar: [
+//       [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+//       [{size: []}],
+//       [{ 'undo': <IoMdUndo/> }, { 'redo': <IoMdRedo /> }], // Specify custom icons for undo and redo buttons
+//       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+//       [{'list': 'ordered'}, {'list': 'bullet'},
+//       {'indent': '-1'}, {'indent': '+1'}],
+//       ['link', 'image', 'video'],
+//       ['clean'],
+//   ],
+//   clipboard: {
+//       // toggle to add extra line breaks when pasting HTML:
+//       matchVisual: false,
+//   }
+// };
 
-const formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'video'
-];
+// const formats = [
+//   'header', 'font', 'size',
+//   'bold', 'italic', 'underline', 'strike', 'blockquote',
+//   'list', 'bullet', 'indent',
+//   'link', 'image', 'video'
+// ];
 
-const Editor = ({
-  blogData,
-  setBlogData,
-}:any) => {
+const Editor = ({ blogData, setBlogData }: any) => {
 
-  useEffect(() => {
-    // Ensure that React Quill is loaded on the client-side
-    const Quill = require('react-quill');
-    if (Quill && typeof window !== 'undefined') {
-        // You can do additional setup here if needed
-    }
-}, []);
 
   return (
     <>
-    <ReactQuill
-      className="editor"
-      modules={modules}
-      formats={formats}
-      theme="snow"
-      value={blogData.content || ""}
-      onChange={(value) => setBlogData({...blogData, content:value})}
-    />
+      <EditorToolbar />
+      <ReactQuill
+        className="editor"
+        modules={modules}
+        formats={formats}
+        theme="snow"
+        value={blogData.content || ""}
+        onChange={(value) => setBlogData({ ...blogData, content: value })}
+      />
     </>
   );
 };
-
-
 
 export default Editor;
