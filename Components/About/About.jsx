5b2fccbf67f88d202615fa9f60608aaba84f214a
rutvik771 <patelrutvik039@gmail.com>
2024-04-27 12:57:35 +0530

    fix responsive for about us page

--- Components/About/About.jsx
+++ Components/About/About.jsx
@@ -16,14 +16,14 @@ export const About = () => {
           <p className='about-title'>About Me</p>
           </div>
           <div className='main-content gap-x-6 gap-y-6'>
-            <div className=''>
+            <div className='mobile-responsive'>
               <div>
                 <p>Hello! My name is Rutvik Patel, and I specialize in building web applications using the MERN (MongoDB, Express, React, and Node.js) technology stack.</p>
                 <p className='py-2'>I am passionate about my work and enjoy tackling complex challenges. I believe that communication, collaboration, and attention to detail are key factors in delivering successful projects. Therefore, I always work closely with my clients to understand their specific requirements, provide regular updates, and ensure that the final product exceeds their expectations.</p>
               </div>
               <div className='py-3'>
                 <p>Here are a few technologies I’ve been working with recently:</p>
-                <ul className='grid grid-cols-2 gap-2 m-4 list-disc secondary-text'>
+                <ul className='grid grid-cols-2 gap-2 m-4 list-none secondary-text'>
                   <li>Java-script</li>
                   <li>React Js</li>
                   <li>Next Js</li>
